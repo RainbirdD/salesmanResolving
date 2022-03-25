dfd93c7ae0fb972d48cd5d1fc7ff86b47f38d9d3
Oleg Belous <javelineua@gmail.com>
2022-03-25 19:14:07 +0200

    Added algorhytm/html

--- src/main/java/com/salesman/Controllers/CoordinateController.java
+++ src/main/java/com/salesman/Controllers/CoordinateController.java
@@ -2,15 +2,16 @@ package com.salesman.Controllers;
 
 import com.salesman.Entity.Coordinates;
 import com.salesman.Repository.CoordinatesRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 
-@RestController
-@RequiredArgsConstructor
+@Controller
 public class CoordinateController {
     @Autowired
     CoordinatesRepository coordinatesRepository;
@@ -19,4 +20,17 @@ public class CoordinateController {
     List<Coordinates> all(){
         return coordinatesRepository.findAll();
     }
+
+    @GetMapping("/home")
+    public String showGraph(Model model){
+        var scope = List.of(83, 56, 97);
+        var coodA = List.of(2, 3, 6);
+        var coodB = List.of(1, 5, 7);
+        var time = List.of(1, 2, 3);
+        model.addAttribute("scope", scope);
+        model.addAttribute("time", 2);
+        return "home";
+
+    }
+
 }
