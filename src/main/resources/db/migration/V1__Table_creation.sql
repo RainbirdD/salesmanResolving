b32fad0ad47b0d963d6b43bb0d6aed7a60f0d7d8
Oleg Belous <javelineua@gmail.com>
2022-05-24 17:29:27 +0300

    Dynamic towns

--- src/main/resources/db/migration/V1__Table_creation.sql
+++
@@ -1,6 +0,0 @@
--- CREATE TABLE coordinates(
---                         id SERIAL PRIMARY KEY ,
---                         revision int,
---                         dotX varchar(255),
---                         dotY varchar(255)
--- );
