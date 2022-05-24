242e9d0affc68d21cb2a264ca934a3a9eed0cd69
Oleg Belous <javelineua@gmail.com>
2022-05-24 19:42:10 +0300

    Added city acrual coordinates

--- src/main/java/com/salesman/Controllers/CoordinateController.java
+++ src/main/java/com/salesman/Controllers/CoordinateController.java
@@ -28,18 +28,26 @@ public class CoordinateController {
         System.out.println("Exec");
         //Towns
         int numberOfCities = 15;
-        int[][] travelPrices = new int[numberOfCities][numberOfCities];
-        for (int i = 0; i < numberOfCities; i++) {
-            for (int j = 0; j <= i; j++) {
-                Random rand = new Random();
-                if (i == j)
-                    travelPrices[i][j] = 0;
-                else {
-                    travelPrices[i][j] = rand.nextInt(100);
-                    travelPrices[j][i] = travelPrices[i][j];
-                }
-            }
-        }
+
+        int[][] travelPrices = new int[][]{
+                {0, 8938, 5628, 6189, 6604, 7773, 9045, 7442, 7637, 5860, 7850, 7482, 5260, 5775, 7311},
+                {8938, 0, 3800, 1507, 2801, 2479, 2002, 3758, 4004, 3610, 5818, 4811, 4155, 3494, 2280},
+                {5628, 3800, 0, 732, 1035, 2147, 3442, 2220, 2626, 331, 3970, 2952, 379, 351, 3315},
+                {6189, 1507, 732, 0, 956, 1627, 2948, 1488, 1893, 447, 3315, 2256, 999, 563, 3945},
+                {6604, 2801, 1035, 956, 0, 1281, 2483, 1913, 2334, 809, 4028, 2920, 1416, 703, 3045},
+                {7773, 2479, 2147, 1627, 1282, 0, 1320, 1286, 1586, 1827, 3438, 2369, 2512, 1802, 3674},
+                {9045, 2002, 3442, 2948, 2483, 1320, 0, 2227, 2311, 3131, 3972, 3097, 3814, 3091, 3947},
+                {7442, 3758, 2220, 1488, 1913, 1286, 2227, 0, 425, 1912, 2198, 1097, 2470, 3703, 4819},
+                {7637, 4004, 2626, 1893, 2334, 1586, 2311, 425, 0, 2143, 1852, 809, 2858, 2406, 5199},
+                {5860, 3610, 331, 447, 809, 1827, 3131, 1912, 2143, 0, 3430, 2419, 714, 549, 4012},
+                {7850, 5818, 3970, 3315, 4028, 3438, 3972, 2198, 1852, 3430, 0, 1114, 4057, 3876, 7012},
+                {7482, 4811, 2952, 2256, 2920, 2369, 3097, 1097, 809, 2419, 1114, 0, 3094, 2816, 5899},
+                {5260, 4155, 379, 999, 1416, 2512, 3814, 2470, 2858, 714, 4057, 3094, 0, 730, 3798},
+                {5775, 3494, 351, 563, 703, 1802, 3091, 3703, 2406, 549, 3876, 2816, 730, 0, 3463},
+                {7311, 2280, 3315, 3945, 3045, 3674, 3947, 4819, 5199, 4012, 7012, 5899, 3798, 3463, 0}
+
+        };
+
 
         UberSalesmensch geneticAlgorithmWithRoulette = new UberSalesmensch(numberOfCities, SelectionType.ROULETTE, travelPrices, 0, 0);
         UberSalesmensch geneticAlgorithmWithTour = new UberSalesmensch(numberOfCities, SelectionType.TOURNAMENT, travelPrices, 0, 0);
@@ -78,7 +86,6 @@ public class CoordinateController {
                     .sorted(Comparator.comparing(Map.Entry<Integer, Long>::getValue).reversed())
                     .map(Map.Entry<Integer, Long>::getValue)
                     .collect(Collectors.toList());
-
 
 
             roulette.add(new GenomeLoop(res.getSalesmanGenome().getFitness(), generationRes, key, value, coordinates));
