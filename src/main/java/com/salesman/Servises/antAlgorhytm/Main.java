701504c08a579b0fa17f1f8c9c3bc6594c86f0a2
Oleg Belous <javelineua@gmail.com>
2022-05-29 13:38:33 +0300

    fixes

--- src/main/java/com/salesman/Servises/antAlgorhytm/Main.java
+++
@@ -1,9 +0,0 @@
-package com.salesman.Servises.antAlgorhytm;
-
-public class Main {
-    public static void main(String[] args) {
-
-        AntAlgorithm TSP = new AntAlgorithm(100L,10L,20L,5,5000,50,5,100,15);
-        TSP.solveXXX();
-    }
-}
